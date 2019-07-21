db06942cd68cf27d709a4e53b556fe3a60150a43
Tomasz Domagała <tomekdo3@gmail.com>
2019-07-21 21:02:43 +0200

    Updates

---
+++ precache-manifest.56c16fd132cc85427ca1a93ab4f32faa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8445522746147e83cb20203b97332737",
+    "url": "/MachineLearningStorage/index.html"
+  },
+  {
+    "revision": "0803d24915c8ac4b2288",
+    "url": "/MachineLearningStorage/static/css/main.8c769c54.chunk.css"
+  },
+  {
+    "revision": "90869d035ad80677c361",
+    "url": "/MachineLearningStorage/static/js/2.a03ca3c1.chunk.js"
+  },
+  {
+    "revision": "0803d24915c8ac4b2288",
+    "url": "/MachineLearningStorage/static/js/main.57ee44ed.chunk.js"
+  },
+  {
+    "revision": "1cb2c6585a0c968f317c",
+    "url": "/MachineLearningStorage/static/js/runtime~main.3a686455.js"
+  }
+]);
